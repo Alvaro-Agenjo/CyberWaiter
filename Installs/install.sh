e1377a4ac9bdbda40d6023fad63fe4fbd873c430
Alvaro-Agenjo <proyectomidas02@gmail.com>
2026-03-19 13:29:25 +0100

    Partial install for user already with ROS2 Humble

--- Installs/install.sh
+++ Installs/install.sh
@@ -64,7 +64,7 @@ echo -e "\e[36m---------Docker configuration---------\e[0m \n"
 sudo groupadd Docker
 sudo usermod -aG docker ${USER}
 sudo chmod 666 /var/run/docker.sock
-docker network create --subnet=192.168.56.0/24 ursim_net
+#docker network create --subnet=192.168.56.0/24 ursim_net
 
 
 echo -e "\e[36m---------Instalacion Finalizada---------\e[0m \n" 
